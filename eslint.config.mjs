154d32561208de8abcda6bf9ec6be9feffc1e25e
lerethel <32931827+lerethel@users.noreply.github.com>
2024-08-19 01:38:19 +0300

    Refactor the JS code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, Toastify: "readonly", Mark: "readonly" },
+    },
+  },
   pluginJs.configs.recommended,
 ];
