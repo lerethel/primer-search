00220474a21951e2b530e66bd26b0409ebc9c6b4
lerethel <32931827+lerethel@users.noreply.github.com>
2024-08-31 21:00:06 +0300

    Set the order of selectors for Primer-BLAST
    Use a Map to set the order in which the selectors are run since some
    error pages on Primer-BLAST seem to have an empty #alignments element

--- src/server/controllers/blast-controller.js
+++ src/server/controllers/blast-controller.js
@@ -28,49 +28,54 @@ const blastURL =
 const rprimerField = /^(?:(?:forward|reverse) primer|product length)$/i;
 const blastPrefix = "Primer-BLAST: ";
 
-const senderBySelector = {
-  "#statInfo"(req, res, element) {
-    const statusMessage = element.getElementsByTagName("td")[1].textContent;
-    res.status(202).json({
-      message: blastPrefix + cleanMessage(statusMessage),
-    });
-  },
-  "#alignments"(req, res, element) {
-    const entries = element
-      .getElementsByTagName("th")
-      .filter((th) => rprimerField.test(th.textContent));
-    const getPrimerProperty = (index) =>
-      entries[index].nextElementSibling.textContent;
-    const primerInfo = [];
+// Some error pages on Primer-BLAST seem to have an empty #alignments element.
+// Use a Map to control the order in which the selectors are run.
+const senderBySelector = new Map();
 
-    for (let i = 0; i < entries.length; ) {
-      primerInfo.push({
-        forward: getPrimerProperty(i++),
-        reverse: getPrimerProperty(i++),
-        length: getPrimerProperty(i++),
-      });
-    }
+senderBySelector.set(".error, .info", (req, res, element) => {
+  const message = element?.textContent;
+  res.status(404).json({
+    message: message
+      ? blastPrefix + cleanMessage(message)
+      : "Unknown error occurred.",
+  });
+});
 
-    res.status(200).json(primerInfo);
-  },
-  "#userGuidedForm"(req, res) {
-    const jobURL = `${blastURL}?job_key=${req.query.job_key}`;
-    res.status(202).json({
-      message:
-        "Primer-BLAST needs user guidance. Follow " +
-        "the link below to resolve the issue. You can " +
-        `return to this page after submitting the form.\n\n${jobURL}`,
+senderBySelector.set("#statInfo", (req, res, element) => {
+  const statusMessage = element.getElementsByTagName("td")[1].textContent;
+  res.status(202).json({
+    message: blastPrefix + cleanMessage(statusMessage),
+  });
+});
+
+senderBySelector.set("#userGuidedForm", (req, res) => {
+  const jobURL = `${blastURL}?job_key=${req.query.job_key}`;
+  res.status(202).json({
+    message:
+      "Primer-BLAST needs user guidance. Follow " +
+      "the link below to resolve the issue. You can " +
+      `return to this page after submitting the form.\n\n${jobURL}`,
+  });
+});
+
+senderBySelector.set("#alignments", (req, res, element) => {
+  const entries = element
+    .getElementsByTagName("th")
+    .filter((th) => rprimerField.test(th.textContent));
+  const getPrimerProperty = (index) =>
+    entries[index].nextElementSibling.textContent;
+  const primerInfo = [];
+
+  for (let i = 0; i < entries.length; ) {
+    primerInfo.push({
+      forward: getPrimerProperty(i++),
+      reverse: getPrimerProperty(i++),
+      length: getPrimerProperty(i++),
     });
-  },
-  ".error, .info"(req, res, element) {
-    const message = element?.textContent;
-    res.status(404).json({
-      message: message
-        ? blastPrefix + cleanMessage(message)
-        : "Unknown error occurred.",
-    });
-  },
-};
+  }
+
+  res.status(200).json(primerInfo);
+});
 
 export async function initSearch(req, res) {
   const { sequence, species, onSpliceSite, maxProductLength } = req.body;
@@ -88,7 +93,7 @@ export async function initSearch(req, res) {
 
   if (!retryURL) {
     const document = await responseToDOM(response);
-    return senderBySelector[".error, .info"](
+    return senderBySelector.get(".error, .info")(
       req,
       res,
       document.querySelector(".error, .info")
@@ -102,10 +107,10 @@ export async function getPrimers(req, res) {
   const response = await fetch(`${blastURL}?job_key=${req.query.job_key}`);
   const document = await responseToDOM(response);
 
-  for (const selector in senderBySelector) {
+  for (const [selector, sender] of senderBySelector) {
     const element = document.querySelector(selector);
     if (element) {
-      return senderBySelector[selector](req, res, element);
+      return sender(req, res, element);
     }
   }
 }
