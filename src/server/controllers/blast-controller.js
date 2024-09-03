69f043f71ed611ae7de63ae87f18e2f1f16991ac
lerethel <32931827+lerethel@users.noreply.github.com>
2024-09-03 15:30:30 +0300

    Add aliases for selectors

--- src/server/controllers/blast-controller.js
+++ src/server/controllers/blast-controller.js
@@ -30,11 +30,18 @@ const blastURL =
 const rprimerField = /^(?:(?:forward|reverse) primer|product length)$/i;
 const blastPrefix = "Primer-BLAST: ";
 
+const selectorAlias = {
+  status: "#statInfo",
+  primer: "#alignments",
+  error: ".error, .info",
+  default: "html",
+};
+
 // The selectors aren't mutually exclusive.
 // Use a Map to control the order in which they are run, along with a validity check.
 const parserBySelector = new Map();
 
-parserBySelector.set("#statInfo", (req, res, element) => {
+parserBySelector.set(selectorAlias.status, (req, res, element) => {
   const message = element.getElementsByTagName("td")[1].textContent;
 
   return {
@@ -44,7 +51,7 @@ parserBySelector.set("#statInfo", (req, res, element) => {
   };
 });
 
-parserBySelector.set("#alignments", (req, res, element) => {
+parserBySelector.set(selectorAlias.primer, (req, res, element) => {
   const entries = element
     .getElementsByTagName("th")
     .filter((th) => rprimerField.test(th.textContent));
@@ -63,7 +70,7 @@ parserBySelector.set("#alignments", (req, res, element) => {
   return { status: 200, valid: primerInfo.length > 0, data: primerInfo };
 });
 
-parserBySelector.set(".error, .info", (req, res, element) => {
+parserBySelector.set(selectorAlias.error, (req, res, element) => {
   const message = element.textContent;
 
   return {
@@ -75,7 +82,7 @@ parserBySelector.set(".error, .info", (req, res, element) => {
 
 // The default option, which should always be at the end,
 // be valid, and include an element that is always in the HTML page.
-parserBySelector.set("html", () => {
+parserBySelector.set(selectorAlias.default, () => {
   return {
     status: 404,
     valid: true,
@@ -112,10 +119,10 @@ export async function initSearch(req, res) {
   const retryURL = response.headers.get("x-ncbi-retry-url");
 
   if (!retryURL) {
-    const { status, data } = parserBySelector.get(".error, .info")(
+    const { status, data } = parserBySelector.get(selectorAlias.error)(
       req,
       res,
-      (await responseToDOM(response)).querySelector(".error, .info")
+      (await responseToDOM(response)).querySelector(selectorAlias.error)
     );
     return res.status(status).json(data);
   }
@@ -161,7 +168,7 @@ export async function getPrimers(req, res) {
       continue;
     }
 
-    if (selector === "#alignments") {
+    if (selector === selectorAlias.primer) {
       db.run("UPDATE primer SET data = ? WHERE job_key = ?;", [
         JSON.stringify(data),
         jobKey,
