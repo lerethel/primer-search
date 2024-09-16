40293f7e0d74f9c37f0abd843c0af7b4694a2cad
lerethel <32931827+lerethel@users.noreply.github.com>
2024-09-16 15:13:14 +0300

    Migrate to express 5.0 and update other dependencies

--- src/server/routes/index-router.js
+++ src/server/routes/index-router.js
@@ -3,7 +3,7 @@ import * as indexController from "../controllers/index-controller.js";
 
 const router = express.Router();
 
-router.get("^/$|/index(.html)?", indexController.getIndex);
-router.get("/*", indexController.getNotFound);
+router.get(/^(?:\/|\/index(?:\.html)?)$/i, indexController.getIndex);
+router.get(/.*/, indexController.getNotFound);
 
 export default router;
